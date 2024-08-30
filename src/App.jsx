25fc91be624a2555f7a971cf85a3bf9d5761eddd
Valou-22 <valounovadev@outlook.be>
2024-08-30 22:03:01 +0200

    add episodes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,46 @@
 import { useState } from "react";
-import { episodes } from "./utils/series";
+import { emilyInParis } from "./utils/series";
 
 function App() {
   const [indexEp, setIndexEp] = useState(0);
+  const [indexSeas, setIndexSeas] = useState(0);
+
+  const seasons = emilyInParis.seasons;
+
+  console.log(`saison : ${indexSeas + 1} episode : ${indexEp + 1}`);
 
   return (
     <div className="h-screen flex w-full justify-center items-center flex-col gap-10">
       <h1 className="text-white font-bold text-4xl">Série pour les bg</h1>
       <form>
         <select
-          onChange={(e) => setIndexEp(e.target.selectedIndex)}
+          onChange={(e) => {
+            setIndexSeas(e.target.selectedIndex);
+            setIndexEp(0); // Réinitialiser l'épisode au changement de saison
+          }}
+        >
+          {seasons.map((season, index) => (
+            <option key={index} value={index}>
+              Saison {index + 1}
+            </option>
+          ))}
+        </select>
+        <select
+          onChange={(e) => {
+            setIndexEp(e.target.selectedIndex);
+          }}
           name=""
           id=""
         >
-          {episodes.map((episode, index) => (
+          {seasons[indexSeas].episodes.map((episode, index) => (
             <option key={index} value={index}>
               Episode {index + 1}
             </option>
           ))}
         </select>
       </form>
-      <video key={indexEp} width="640" height="360" controls>
-        <source src={episodes[indexEp]} type="video/mp4" />
+      <video key={`${indexSeas}-${indexEp}`} width="640" height="360" controls>
+        <source src={seasons[indexSeas].episodes[indexEp]} type="video/mp4" />
         Votre navigateur ne supporte pas la lecture de cette vidéo.
       </video>
     </div>
--- src/utils/series.js
+++ src/utils/series.js
@@ -1,6 +1,29 @@
-export const episodes = [
-  "https://g8l2qa51m5x1f.pradoi.com/v/01/04233/ucl1f4nc7m87_x/Emily.in.Paris.S01E01.FRENCH.1080p.WEB.H264.Wawacity.vip_x.mp4?t=mW9saif6Z6rR7i4B32adz_Ick3z7658t8oGsv_Lfjbs&s=1725043104&e=129600&f=21165805&sp=1000&i=0.0",
-  "https://ocuoxflke2uo1a.pradoi.com/v/01/04233/x83j6td4rrw3_x/Emily.in.Paris.S01E02.FRENCH.1080p.WEB.H264.Wawacity.vip_x.mp4?t=2xJU_SB_2Kf9ZO0ACNEKzZTo9IY-eu3tE8PzicdNzgI&s=1725042794&e=129600&f=21165796&sp=1000&i=0.0",
-  "https://8ulqhwjgxdzbe.pradoi.com/v/01/04233/ro9hdewa75pc_x/Emily.in.Paris.S01E03.FRENCH.1080p.WEB.H264.Wawacity.vip_x.mp4?t=SDnqmD6YDN_hHo3g4J7jwZ0mv25w6HCcV2OfLk_offk&s=1725044140&e=129600&f=21165797&sp=1000&i=0.0",
-  "https://fvynu5an54z0eb.pradoi.com/v/01/04233/zm3wkbboyjan_x/Emily.in.Paris.S01E04.FRENCH.1080p.WEB.H264.Wawacity.vip_x.mp4?t=JcVMwva2xjtER1JCF7s6gd_EO3pXJcoYWHyiQlQe2fU&s=1725044310&e=129600&f=21165804&sp=1000&i=0.0",
-];
+export const emilyInParis = {
+  title: "Emily in Paris",
+  seasons: [
+    {
+      episodes: [
+        "https://g8l2qa51m5x1f.pradoi.com/v/01/04233/ucl1f4nc7m87_x/Emily.in.Paris.S01E01.FRENCH.1080p.WEB.H264.Wawacity.vip_x.mp4?t=mW9saif6Z6rR7i4B32adz_Ick3z7658t8oGsv_Lfjbs&s=1725043104&e=129600&f=21165805&sp=1000&i=0.0",
+        "https://ocuoxflke2uo1a.pradoi.com/v/01/04233/x83j6td4rrw3_x/Emily.in.Paris.S01E02.FRENCH.1080p.WEB.H264.Wawacity.vip_x.mp4?t=2xJU_SB_2Kf9ZO0ACNEKzZTo9IY-eu3tE8PzicdNzgI&s=1725042794&e=129600&f=21165796&sp=1000&i=0.0",
+        "https://8ulqhwjgxdzbe.pradoi.com/v/01/04233/ro9hdewa75pc_x/Emily.in.Paris.S01E03.FRENCH.1080p.WEB.H264.Wawacity.vip_x.mp4?t=SDnqmD6YDN_hHo3g4J7jwZ0mv25w6HCcV2OfLk_offk&s=1725044140&e=129600&f=21165797&sp=1000&i=0.0",
+        "https://fvynu5an54z0eb.pradoi.com/v/01/04233/zm3wkbboyjan_x/Emily.in.Paris.S01E04.FRENCH.1080p.WEB.H264.Wawacity.vip_x.mp4?t=JcVMwva2xjtER1JCF7s6gd_EO3pXJcoYWHyiQlQe2fU&s=1725044310&e=129600&f=21165804&sp=1000&i=0.0",
+        "https://81cjh64633yue.pradoi.com/v/01/04233/tu42pr5ecqhx_x/Emily.in.Paris.S01E05.FRENCH.1080p.WEB.H264.Wawacity.vip_x.mp4?t=bcb-ocDoLmt7MTpyVWjgWBNrXOOHFMVCCbpBt19FLcA&s=1725045944&e=129600&f=21165811&sp=1000&i=0.0",
+        "https://g8l2qa51m5x1f.pradoi.com/v/01/04233/1qrlzd181tu1_x/Emily.in.Paris.S01E06.FRENCH.1080p.WEB.x264.Wawacity.vip_x.mp4?t=scmRZhwcal_EuLJeAn6Jkp3zZkdIsxzHJb2A4bE6jQU&s=1725047242&e=129600&f=21165834&sp=1000&i=0.0",
+        "https://g8l2qa51m5x1f.pradoi.com/v/01/04233/kwasn8osynr0_x/Emily.in.Paris.S01E07.FRENCH.1080p.WEB.x264.Wawacity.vip_x.mp4?t=q9uDYlTh7emXaXce9q3iI1wzKT4YrEYUYwGhYt7EA8o&s=1725047664&e=129600&f=21165883&sp=1000&i=0.0",
+        "https://ocuoxflke2uo1a.pradoi.com/v/01/04233/vwwd95bgfkso_x/Emily.in.Paris.S01E08.FRENCH.1080p.WEB.x264.Wawacity.vip_x.mp4?t=4dMnKgOKyT8ITaLUe9sEn4blr1rNHKGI5nekYm1fAEc&s=1725047839&e=129600&f=21165889&sp=1000&i=0.0",
+        "https://ocuoxflke2uo1a.pradoi.com/v/01/04233/l5bi8fvth5av_x/Emily.in.Paris.S01E09.FRENCH.1080p.WEB.x264.Wawacity.vip_x.mp4?t=3ZzUAg11lLlbRZma4SbtgoH4O1pd_57-bi4Hg-PZRnM&s=1725047906&e=129600&f=21165878&sp=1000&i=0.0",
+        "https://8ulqhwjgxdzbe.pradoi.com/v/01/04233/ipl97pxndwgl_x/Emily.in.Paris.S01E10.FiNAL.FRENCH.1080p.WEB.x264.Wawacity.vip_x.mp4?t=_295-PznfI8I7TVzkYZNny3av4JkI60VZ4OYHv65nEY&s=1725047937&e=129600&f=21165929&sp=1000&i=0.0",
+        // Ajouter plus d'URL d'épisodes ici...
+      ],
+    },
+    {
+      episodes: [
+        "https://p1yiis79s8azw.pradoi.com/v/01/04233/zmxp0lml3hqw_x/Emily.in.Paris.S02E01.FRENCH.1080p.WEB.x264.Wawacity.work_x.mp4?t=TenCFd0_bygkIsq8LiXRcU4Galvr2JMg6JBRsiENYLI&s=1725046772&e=129600&f=21165895&sp=1000&i=0.0",
+        "https://g8l2qa51m5x1f.pradoi.com/v/01/04233/ocpvc2861npr_x/Emily.in.Paris.S02E02.FRENCH.1080p.WEB.x264.Wawacity.work_x.mp4?t=-EywyPqQtEn8-a60GzGyCpS27_-kXFlT2YKMyGMKyTE&s=1725048036&e=129600&f=21165906&sp=1000&i=0.0",
+        "https://fvynu5an54z0eb.pradoi.com/v/01/04233/5bhh8eisiw4r_x/Emily.in.Paris.S02E03.FRENCH.1080p.WEB.x264.Wawacity.work_x.mp4?t=YRL1DJ2EptjXrFj4UHgBXWH5VL6B3BobYThpDQLdWkw&s=1725048087&e=129600&f=21165902&sp=1000&i=0.0",
+        "https://81cjh64633yue.pradoi.com/v/01/04233/z9wvvvvkykol_x/Emily.in.Paris.S02E04.FRENCH.1080p.WEB.x264.Wawacity.work_x.mp4?t=9PicbhVqaB5SUQkkR_bKM9TUzMG2i--sr8wxp29UozY&s=1725048166&e=129600&f=21165938&sp=1000&i=0.0",
+      ],
+    },
+    // Ajouter plus de saisons ici...
+  ],
+};
